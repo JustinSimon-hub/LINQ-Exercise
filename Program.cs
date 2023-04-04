f019fa4b1fb2176c959acfef63e55f9819576f68
Justin Simon <justinsimon@Justins-MBP.lan>
2023-04-03 20:06:56 -0700

    Finished Assignment

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace LINQ
+
+
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           
+            List<string> VidGames = new List<string> { "Zelda", "Halo", "Gears of War" };
+           
+       var ans = VidGames.OrderBy(s => s.Length);
+        foreach( string i in ans)
+        {
+            Console.WriteLine(i); 
+        }
+
+
+           
+           
+
+
+        }
+    }
+
+   
+}
