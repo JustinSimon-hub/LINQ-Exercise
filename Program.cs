28931dae957666400fa64539baf784a69420db2b
Justin Simon <justinsimon@Justins-MBP.lan>
2023-05-12 17:15:19 -0700

    Second run of assingment

--- Program.cs
+++ Program.cs
@@ -2,27 +2,32 @@
 
 
 {
+    namespace LINQ
+{
     public class Program
     {
         static void Main(string[] args)
         {
-           
-           
-            List<string> VidGames = new List<string> { "Zelda", "Halo", "Gears of War" };
-           
-       var ans = VidGames.OrderBy(s => s.Length);
-        foreach( string i in ans)
-        {
-            Console.WriteLine(i); 
-        }
-
-
-           
-           
-
-
+           var vidgames = new List<string>{"Zelda" , "Halo", "Gears of War"};
+           var sortedgames = vidgames.OrderBy(x => x.Length);
+           foreach(var i in sortedgames)
+           {
+            Console.WriteLine(i);
+            Console.WriteLine("----------");
+           }
         }
     }
-
-   
 }
+}
+
+
+
+/*
+Use the list of [Linq methods](https://portal.truecoders.io/course/1/lecture/62/lesson) 
+for help
+
++ Create a list of video game names...
++ Order the list of games by length of the game name.
++ Use the lambda expression in this exercise as well.
++ use Method Syntax for this exercise
+*/
